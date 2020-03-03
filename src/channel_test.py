97856eb3713818dc20fd61a17d4ba8ad91cf3d0b
Jackie Cai <z5259449@ad.unsw.edu.au>
2020-03-03 21:36:51 +1100

    change the 1 to channel1 to put into a variable

--- src/channel_test.py
+++ src/channel_test.py
@@ -14,17 +14,17 @@ def test_channel_create():
 def test_channel_list():
 #test if the channel list returns id 1 and the name 'my channel'
     user1 = auth_register('name@mail.com', 'password', 'Jim', 'Smith')
-    channels_create(user1['token'], 'My Channel', True)
+    channel1 = channels_create(user1['token'], 'My Channel', True)
     channelList = channels_list(user1['token'])
-    assert channelList['channel_id'] == 1
+    assert channelList['channel_id'] == channel1['channel_id']
     assert channelList['name'] == 'My Channel'
 
 def test_channel_list_all():
 #same as channel_list but add one more channel
     user1 = auth_register('name@mail.com', 'password', 'Jim', 'Smith')
-    channels_create(user1['token'], 'My Channel', True)
+    channel1 = channels_create(user1['token'], 'My Channel', True)
     channelList = channels_listall(user1['token'])
-    assert channelList['channel_id'] == 1
+    assert channelList['channel_id'] == channel1['channel_id']
     assert channelList['name'] == 'My Channel'
 
 def test_channel_addowner():
