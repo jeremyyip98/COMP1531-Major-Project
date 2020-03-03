c1474cdc86fcecc7bbb7f92c34d3fb585a6b3012
Jackie Cai <z5259449@ad.unsw.edu.au>
2020-03-03 19:47:01 +1100

    added test for details but need to change last line to check if user2 has been added

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,6 +1,7 @@
 from error import AccessError, InputError
 from auth import auth_register
 from channels import *
+from channel import *
 import pytest
 
 
@@ -26,6 +27,38 @@ def test_channel_list_all():
     assert channelList['channel_id'] == 1
     assert channelList['name'] == 'My Channel'
 
+def test_channel_addowner():
+    #assume user1 is owner of channel when he makes the channel
+    user1 = auth_register('name@mail.com', 'password', 'Jim', 'Smith')
+    user2 = auth_register('anme2@mail.com', 'password1', 'Jim', 'Lift')
+    channel1 = channels_create(user1['token'], 'My Channel', True)
+
+    #test if the channel adds user1 again it gives InputError
+    with pytest.raises(InputError) as e:
+        channel_addowner(user1['token'], channel1['channel_id'], user1['u_id'])
+    
+    #test if the channel tries to add channel_id 2 (invalid id)
+    with pytest.raises(InputError) as e:
+        channel_addowner(user1['token'], 2, user1['u_id'])
+    
+    #invites user2 to channel
+    channel_invite(user1['token'],channel1['channel_id'], user2['u_id'])
+
+    #test if the unauthorises user tries to add himself as owner
+    with pytest.raises(AccessError) as e:
+        channel_addowner(user2['token'], channel1['channel_id'], user2['u_id'])
+    
+    #adds user2 as owner
+    channel_addowner(user1['token'], channel1['channel_id'], user2['u_id'])
+
+    details = channel_details(user2['u_id'], channel1['channel_id'])
+    #assert that user 2 is in owners
+    assert 'user2' in details['owner_members']:
+
+
+
+
+    
 
     
 
