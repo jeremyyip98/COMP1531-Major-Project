216199934e92fb14d488b52133194094b10d6fc0
Jackie Cai <z5259449@ad.unsw.edu.au>
2020-03-04 00:01:42 +1100

    more editting to access numbers

--- src/channel_test.py
+++ src/channel_test.py
@@ -71,9 +71,35 @@ def test_channel_addowner():
     user2_profile == user_profile(user2['token'], user2['u_id'])
     assert details['owner_members'][1]['name_first'] == user2_profile['user']['name_first']
 
-def test_channel_details():
-    
-    
+def test_channel_remove_owner():
+    #assume user1 is owner of channel when he makes the channel
+    user1 = auth_register('name@mail.com', 'password', 'Jim', 'Smith')
+    user2 = auth_register('name2@mail.com', 'password1', 'Tim', 'Lift')
+    channel1 = channels_create(user1['token'], 'My Channel', True)
+
+    #test invalid id channel channel_id 10231 
+    with pytest.raises(InputError) as e:
+        channel_removeowner(user1['token'], 10231, user1['u_id'])
+    #test remove owner when user2 is not owner
+    with pytest.raises(InputError) as e:
+        channel_removeowner(user1['token'], channel1['channel_id'], user2['u_id'])
+    #test remove owner when user2 treis to remove user1
+    with pytest.raises(AccessError) as e:
+        channel_removeowner(user2['token'], channel1['channel_id'], user1['u_id'])
+
+    channel_addowner(user1['token'], channel1['channel_id'], user2['u_id'])
+    #checking that the owners are user1 and user2
+    user1_profile == user_profile(user1['token'], user1['u_id'])
+    assert details['owner_members'][0]['name_first'] == user1_profile['user']['name_first']
+
+    user2_profile == user_profile(user2['token'], user2['u_id'])
+    assert details['owner_members'][1]['name_first'] == user2_profile['user']['name_first']
+    assert len(details['owner_member']) == 2
+
+    channel_removeowner(user2['token'], channel1['channel_id'], user2['user_id'])
+    #check that after removing the person the second 
+    assert len(details['owner_members']) == 1
+
 
 
 
