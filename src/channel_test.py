bea1ad7eb80d537c66aaac7ea116dea054a154f3
Jackie Cai <z5259449@ad.unsw.edu.au>
2020-03-03 19:08:55 +1100

    added channel list test have to add another channel for test all

--- src/channel_test.py
+++ src/channel_test.py
@@ -1,10 +1,33 @@
 from error import AccessError, InputError
 from auth import auth_register
-from channels import channels_create
+from channels import *
+import pytest
+
 
 def test_channel_create():
+#test if there is an input error when character string is greater than 20
     user1 = auth_register('name@mail.com', 'password', 'Jim', 'Smith')
     with pytest.raises(InputError) as e:
         channels_create(user1['token'], 'a' * 21, True)
+
+def test_channel_list():
+#test if the channel list returns id 1 and the name 'my channel'
+    user1 = auth_register('name@mail.com', 'password', 'Jim', 'Smith')
+    channels_create(user1['token'], 'My Channel', True)
+    channelList = channels_list(user1['token'])
+    assert channelList['channel_id'] == 1
+    assert channelList['name'] == 'My Channel'
+
+def test_channel_list_all():
+#same as channel_list but add one more channel
+    user1 = auth_register('name@mail.com', 'password', 'Jim', 'Smith')
+    channels_create(user1['token'], 'My Channel', True)
+    channelList = channels_listall(user1['token'])
+    assert channelList['channel_id'] == 1
+    assert channelList['name'] == 'My Channel'
+
+
+    
+
         
     
