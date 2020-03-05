4f8dc2f273cca22118e6ca40f31bf922e228f234
Jeffrey Yang <jeffrey2999@gmail.com>
2020-03-05 14:44:02 +1100

    Fixed a few errors in user_test.py and added more comments

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -6,6 +6,9 @@ from message import message_send
 def register_valid_user():
     return auth_register("test@gmail.com", "Password", "First", "Last")
 
+# Helper function to register another valid user and return u_id and token
+# This is used to test input errors where a particular value (e.g. email) is already being used by another user
+ Helper function to register another valid user and return u_id and token
 def register_another_valid_user():
     return auth_register("anothertest@gmail.com", "Anotherpassword", "Anotherfirst", "Anotherlast")
 
--- src/user_test.py
+++ src/user_test.py
@@ -6,6 +6,8 @@ from error import InputError
 
 
 # We assume that auth_register works
+
+# For a valid user, the returned profile should match with the details of the user
 def test_user_profile_valid():
     results = register_valid_user()
     profile = user_profile(results["token"], results["u_id"])
@@ -16,6 +18,7 @@ def test_user_profile_valid():
     assert profile["handle_str"] == "firstlast"
 
 # User with u_id has to be a valid user
+# We raise an input error since the u_id is invalid
 def test_user_profile_invalid_user():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
@@ -23,68 +26,72 @@ def test_user_profile_invalid_user():
 
 # We now assume that user_profile works
 
+# Both name_first and name_list need to be strings between 1 and 50 characters in length
 def test_user_profile_setname_valid():
     results = register_valid_user()
     user_profile_setname(results["token"], "Newfirst", "Newlast")
 
-# name_first has to be between 1 and 50 characters in length
+# Here, the new name_first is too short
 def test_user_profile_setname_first_name_too_short():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
-        user_profile_setname(results["token"], "", "Last")
+        user_profile_setname(results["token"], "", "NewLast")
 
+# Here, the new name_first is too long
 def test_user_profile_setname_first_name_too_long():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
         user_profile_setname(results["token"], "a"*51, "Newlast")
 
-# name_last has to be between 1 and 50 characters in length
+# Here, the new name_last is too short
 def test_user_profile_setname_last_name_too_short():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
-        user_profile_setname(results["token"], "First", "")
+        user_profile_setname(results["token"], "NewFirst", "")
 
+# Here, the new name_last is too long
 def test_user_profile_setname_last_name_too_long():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
-        user_profile_setname(results["token"], "First", "b"*51)
+        user_profile_setname(results["token"], "NewFirst", "b"*51)
 
-
+# The new email has to be valid and cannot be already used by another user
 def test_user_profile_setemail_valid():
     results = register_valid_user()
     user_profile_setemail(results["token"], "newtest@gmail.com")
 
-# email has to be valid
+# Here, the new email is invalid
 def test_user_profile_setemail_invalid():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
         user_profile_setemail(results["token"], "invalidemail")
 
-# new email cannot already be used
+# Here, the new email is already being used by another user
 def test_user_profile_setemail_email_already_used():
     results = register_valid_user()
-    results2 = register_another_valid_user
-    profile2 = user_profile(results2["token"], results["u_id"])
+    results2 = register_another_valid_user()
+    profile2 = user_profile(results2["token"], results2["u_id"])
     with pytest.raises(InputError) as e:
         user_profile_setemail(results["token"], profile2["email"])
 
-
+# handle_str must be between 3 and 20 characters and cannot be already used by another user
 def test_user_profile_sethandle_valid():
     results = register_valid_user()
     user_profile_sethandle(results["token"], "newhandle")
 
-# handle_str must be between 3 and 20 characters
+#Here, the new handle_str is too short
 def test_user_profile_sethandle_handle_too_short():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
         user_profile_sethandle(results["token"], "a")
 
+#Here, the new handle_str is too long
 def test_user_profile_sethandle_handle_too_long():
     results = register_valid_user()
     with pytest.raises(InputError) as e:
         user_profile_sethandle(results["token"], "a"*21)
 
-# new handle cannot already be used
+# Here, the new handle is already being used by another user
 def test_user_profile_sethandle_handle_already_used():
     results = register_valid_user()
     results2 = register_another_valid_user()
