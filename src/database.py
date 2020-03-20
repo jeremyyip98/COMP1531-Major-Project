d816a8bd3a3ee9cb009d93555ef56a8fe125d971
Sean <smccghy@gmail.com>
2020-03-20 17:32:17 +1100

    Move Registered user store to database

--- src/database.py
+++ src/database.py
@@ -1,1 +1,14 @@
 u_ids = [0]
+
+registered_users_store = {
+                            'registered_users' : 
+                                [
+                                    {                   
+                                    }    
+                                ],
+                            'hashes': 
+                                [
+                                    {
+                                    }
+                                ]
+                        }
