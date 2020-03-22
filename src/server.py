17809080ece0447e7e7ff8cba56ec18df0be7fbb
Jeremy Yip <jeremyyip98@gmail.com>
2020-03-22 22:49:59 +1100

    Reset server.py back to the original content

--- src/server.py
+++ src/server.py
@@ -4,7 +4,6 @@ from flask import Flask, request
 from flask_cors import CORS
 from error import InputError
 
-
 def defaultHandler(err):
     response = err.get_response()
     print('response', err, err.get_response())
@@ -31,21 +30,6 @@ def echo():
     return dumps({
         'data': data
     })
-
-# Possible routes for message/send (Working in progress)
-@APP.route("/message/send", methods=['POST'])
-def message_send():
-    messages = get_message()
-    if not messages:    # If messages is empty
-        message_id = 0
-    else:
-        most_recent_message = messages[-1]
-        message_id = 
-
-
-    data = request.get_json()
-    messages.append(data)
-    return dumps({})
 
 if __name__ == "__main__":
     APP.run(port=(int(sys.argv[1]) if len(sys.argv) == 2 else 8080))
