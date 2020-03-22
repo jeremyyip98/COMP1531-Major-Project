984f1f72be38e3769e3e4e24c42958f0c9a48d3e
Sean <smccghy@gmail.com>
2020-03-22 18:34:53 +1100

    added example to database

--- src/database.py
+++ src/database.py
@@ -16,7 +16,14 @@ u_ids = [0]
 registered_users_store = {
                             'registered_users' : 
                                 [
-                                  #  {                   
+                                  #  {   
+                                  # 'u_id' : u_id,
+                                  # 'email' : email,
+                                  # 'name_first' : name_first,
+                                  # 'name_last' : name_last,
+                                  # 'hash' : hash,
+                                  # 'token' : token,
+                                  # 'handle_str' :firstlast             
                                   #  }    
                                 ]
 
