b5e637d6717e48d8f1fbf8a237db2d88c50ea5a0
Jeremy Chung Lum Yip <jeremychunglum.yip@student.unsw.edu.au>
2020-03-23 14:55:25 +1100

    Update database.py

--- src/database.py
+++ src/database.py
@@ -32,6 +32,15 @@ registered_users_store = {
 
                         }
 
+message_list = [{
+    # channel_id (int) (extra element that is not mentioned in spec)
+    # message_id (int)
+    # u_id (int)
+    # message (string)
+    # time_created (integer (unix timestamp))
+    # reacts (list of dictionaries)
+    # is_pinned (Boolean?)
+}]
 
 def check_token(token):
     if search_database(token):
@@ -50,3 +59,15 @@ def search_database(token):
       if user['token'] == token:
         return user
     return False
+    
+def reset_message():
+    """This function reset the message and returns nothing"""
+    global message_list
+    message_list = [{
+
+    }]
+
+def get_message():
+    """This function get the list of dictionary of messages and returns it"""
+    global message_list
+    return message_list
