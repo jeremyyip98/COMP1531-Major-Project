e2b652177d98c359d0d5f3c12876542b89dea806
Jeremy <jeremyyip98@gmail.com>
2020-03-24 16:59:31 +1100

    Edited the new added function, channel_add(), so it does not return anything

--- src/message.py
+++ src/message.py
@@ -44,7 +44,7 @@ def message_create(channel_id, u_id, message, time):
 # Helper function for message_create() and get_channel_id()
 def channel_add(channel_id, message_id):
     """This function store a list of dictionaries containing
-    the channel_id with it's corresponding message_ids and return it"""
+    the channel_id with it's corresponding message_ids and return nothing"""
     channel = get_channel()
     if not channel: # If the channel is empty
         dictionary = {
@@ -56,7 +56,6 @@ def channel_add(channel_id, message_id):
         for dict_channel in channel:
             if dict_channel['channel_id'] == channel_id:
                 dict_channel['channel_messages'].append(message_id)
-    return channel
 
 # Helper function for message_send() and message_sendlater()
 def check_joined_channel(token, channel_id):
