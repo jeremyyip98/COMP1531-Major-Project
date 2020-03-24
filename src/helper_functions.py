09c75e780ef215a9abae881494818edfcc40f18d
Sean <smccghy@gmail.com>
2020-03-24 12:06:37 +1100

    Fixed missing files

--- src/helper_functions.py
+++ src/helper_functions.py
@@ -1,6 +1,6 @@
 from auth import auth_register
-#from channels import channels_create
-#from message import message_send
+from channels import channels_create
+from message import message_send
 
 # Helper function to register a valid user and return u_id and token
 def register_valid_user():
