afabb7a9f14e52a46ca6f4cdd86cf57ffb1f4f4b
Jeremy Yip <jeremyyip98@gmail.com>
2020-03-27 10:58:43 +1100

    Copied channel.py from test_channel_branch

--- src/channel.py
+++ src/channel.py
@@ -3,7 +3,7 @@ UNSW COMP1531 Iteration 2
 channel.py
 Written by Jackie Cai z5259449
 """
-from database import channel_ids, list_of_channels, search_database, get_u_id
+from database import channel_ids, list_of_channels, search_database, get_u_id, get_profile
 from error import AccessError, InputError
 
 def generate_channel_id():
@@ -150,3 +150,148 @@ def channels_listall(token):
     else:
         #no access error means they are user and returns all channels
         return list_of_channels['channels']
+        
+"""
+UNSW COMP1531 Iteration 2
+channel.py
+Written by Aaron Lin z5258280
+"""
+
+# Invites user with u_id to join channel with channel_id
+# Added immediately once invited
+def channel_invite(token, channel_id, u_id):
+    # check if the token is valid
+    is_valid = search_database(token)
+    if is_valid is False:
+        raise AccessError(description='Invalid Token')
+        
+    # authorised user is user who is in the channel and invites another user
+    authorised_user = is_valid
+    # invited_user is user who is invited and added to the channel
+    # inputError raised in get_profile() when u_id not a valid user
+    invited_user = get_profile(u_id)
+    
+    found_authorised_user = False
+    found_channel = False
+    for channel in list_of_channels['channels']:
+        # found the right channel and add user to channel
+        if channel['channel_id'] == channel_id:
+            found_channel = True
+            # searching through members in the channel
+            for members in channel['all_members']:
+                # checking if authorised user in the channel
+                if authorised_user == members:
+                    found_authorised_user = True
+                    
+            channel['all_members'].append(invited_user)
+    
+    # no channel with channel_id
+    if found_channel is False:
+        raise InputError(description='Channel_ID does not refer to valid channel')
+        
+    # raise accessError if authorised user is not in channel
+    if found_authorised_user is False:
+        raise AccessError(description='Authorised user not in channel')
+    
+def channel_details(token, channel_id):
+    # check if the token is valid
+    is_valid = search_database(token)
+    if is_valid is False:
+        raise AccessError(description='Invalid Token')
+    
+    # authorised user is user who is in the channel and invites another user
+    authorised_user = is_valid
+    found_channel = False
+    found_authorised_user = False
+    
+    details = {}
+    
+    for channel in list_of_channels['channels']:
+        # found the right channel
+        if channel['channel_id'] == channel_id:
+            # found the channel
+            found_channel = True
+            
+            # check if authorised user is in the channel
+            for members in channel['all_members']:
+                if members == authorised_user:
+                    # authorised user is in the channel
+                    found_authorised_user = True
+                    
+            # setting details to have the details of this channel
+            details['name'] = channel['channel_name']
+            details['owner_members'] = channel['owner_members']
+            details['all_members'] = channel['all_members']
+        
+    if found_channel is False:
+        raise InputError(description='Channel_ID is not a valid channel')
+        
+    if found_authorised_user is False:
+        raise AccessError(description='Authorised user is not a member of channel with channel_id')
+        
+    return details
+    
+def channel_messages(token, channel_id, start):
+    # check if the token is valid
+    is_valid = search_database(token)
+    if is_valid is False:
+        raise AccessError(description='Invalid Token')
+    
+    # authorised user is user who is in the channel and invites another user
+    authorised_user = is_valid
+    found_channel = False
+    found_authorised_user = False
+    num_total_messages = 0
+    message_ids = []
+    end = start
+    
+    # get_channel to get the message_list inside the channel
+    for chan in get_channel():
+        if chan['channel_id'] == channel_id:
+            # total messages = length of channel_messages list
+            num_total_messages = len(chan['channel_messages'])
+            int i = 0
+            int end = chan['channel_messages'][-1]
+            # add from message_list to messages
+            for msg_id in chan['channel_messages']:
+                if (i > 50):
+                    break
+                else if (i <= 50 && msg_id == end):
+                    end = -1
+                else:
+                    message_ids.append(msg_id)
+                    end += 1
+                    
+    # initialise empty messages list
+    messages = []
+    
+    message_list = get_message()   
+    for ids in message_ids:      
+        for msg_dict in message_list:
+            if msg_dict['message_id'] == ids:
+                # adds the message dict to messages list
+                messages.append[msg_dict]
+            
+    if start >= num_total_messages:
+        raise InputError('Start is greater than or equal to total messages in the channel')
+    
+    for channel in list_of_channels['channels']:
+        # found the right channel
+        if channel['channel_id'] == channel_id:
+            # found the channel
+            found_channel = True
+            
+            # check if authorised user is in the channel
+            for members in channel['all_members']:
+                if members == authorised_user:
+                    # authorised user is in the channel
+                    found_authorised_user = True
+            
+            
+    if found_channel is False:
+        raise InputError(description='Channel_ID is not a valid channel')
+        
+    if found_authorised_user is False:
+        raise AccessError(description='Authorised user is not a member of channel with channel_id')
+        
+    return {'messages': messages, 'start': start, 'end': end}
