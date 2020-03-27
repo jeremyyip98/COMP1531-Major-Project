e9841939c30fb90d6d710bf931a7bc8732b73bae
Jackie Cai <z5259449@ad.unsw.edu.au>
2020-03-27 15:09:36 +1100

    ran the testing iteration 1 and fied any errors that were up

--- src/channel.py
+++ src/channel.py
@@ -6,32 +6,6 @@ Written by Jackie Cai z5259449
 from database import channel_ids, list_of_channels, search_database, get_u_id, get_profile, get_channel, get_message
 from error import AccessError, InputError
 
-def generate_channel_id():
-    """Makes a channel id and adds it to the databse"""
-    channel_id = max(channel_ids)
-    channel_id += 1
-    channel_ids.append(channel_id)
-    return channel_id
-def channels_create(token, name, is_public):
-    """Makes a channel with a name and sets it as public or not then it makes the
-    person with the token the owner and first member of the channel"""
-    #Check for access error first
-    is_valid = search_database(token)
-    if is_valid is False:
-        raise AccessError(description='Invalid Token')
-    user = get_u_id(token)
-    if len(name) > 20:
-        raise InputError(description='Channel Name Too Long')
-    re_channel_id = generate_channel_id()
-    new_channel = {
-        'channel_id' : re_channel_id,
-        'channel_name' : name,
-        'is_public' : is_public,
-        'owner_members' : [user],
-        'all_members' : [user],
-    }
-    list_of_channels['channels'].append(new_channel)
-    return re_channel_id
 def channel_join(token, channel_id):
     """gets a token from the user and channel_id that the peron can join
     and adds them to the channel (checks if channel is public to allow join)"""
@@ -76,7 +50,7 @@ def channel_leave(token, channel_id):
                     return
             raise AccessError(description='Not a member of the Channel')
     raise InputError(description="Channel ID is not a valid channel")
-def channel_add_owner(token, channel_id, u_id):
+def channel_addowner(token, channel_id, u_id):
     """Another owner adds a member as owner"""
     is_valid = search_database(token)
     if is_valid is False:
@@ -98,7 +72,7 @@ def channel_add_owner(token, channel_id, u_id):
                 chan['owner_members'].appen(u_id)
                 return
     raise InputError(description='Channel ID is not a valid channel')
-def channel_remove_owner(token, channel_id, u_id):
+def channel_removeowner(token, channel_id, u_id):
     """Another owner removes and owner from the channel but the member is stil in the channel"""
     is_valid = search_database(token)
     if is_valid is False:
@@ -118,31 +92,6 @@ def channel_remove_owner(token, channel_id, u_id):
                 chan['owner_members'].remove(u_id)
                 return
     raise InputError(description='Channel ID is not a valid channel')
-def channels_list(token):
-    """Gets a token and returns a list of channel the user is in"""
-    is_valid = search_database(token)
-    if is_valid is False:
-        raise AccessError(description='Invalid Token')
-    #make a list to store all channels user is in
-    authed_channel = []
-    #get the user's id
-    user = get_u_id(token)
-    #iterate through the list of channels
-    for chan in list_of_channels['channels']:
-        #iterate through list of members
-        for mem in chan['all_members']:
-            #if user are in the channel append to list
-            if mem == user:
-                authed_channel.append(mem)
-    return authed_channel
-def channels_listall(token):
-    """Gets a token and returns a list of channel that exist"""
-    #check for access error
-    is_valid = search_database(token)
-    if is_valid is False:
-        raise AccessError(description='Invalid Token')
-    #no access error means they are user and returns all channels
-    return list_of_channels['channels']
 
 """
 UNSW COMP1531 Iteration 2
@@ -193,7 +142,7 @@ def channel_details(token, channel_id):
         raise AccessError(description='Invalid Token')
     
     # authorised user is user who is in the channel and invites another user
-    authorised_user = is_valid
+    authorised_user = is_valid['u_id']
     found_channel = False
     found_authorised_user = False
     
---
+++ src/channels.py
@@ -0,0 +1,59 @@
+"""
+UNSW COMP1531 Iteration 2
+channel.py
+Written by Jackie Cai z5259449
+"""
+from database import channel_ids, list_of_channels, search_database, get_u_id
+from error import AccessError, InputError
+
+def generate_channel_id():
+    """Makes a channel id and adds it to the databse"""
+    channel_id = max(channel_ids)
+    channel_id += 1
+    channel_ids.append(channel_id)
+    return channel_id
+def channels_create(token, name, is_public):
+    """Makes a channel with a name and sets it as public or not then it makes the
+    person with the token the owner and first member of the channel"""
+    #Check for access error first
+    is_valid = search_database(token)
+    if is_valid is False:
+        raise AccessError(description='Invalid Token')
+    user = get_u_id(token)
+    if len(name) > 20:
+        raise InputError(description='Channel Name Too Long')
+    re_channel_id = generate_channel_id()
+    new_channel = {
+        'channel_id' : re_channel_id,
+        'channel_name' : name,
+        'is_public' : is_public,
+        'owner_members' : [user],
+        'all_members' : [user],
+    }
+    list_of_channels['channels'].append(new_channel)
+    return re_channel_id
+def channels_list(token):
+    """Gets a token and returns a list of channel the user is in"""
+    is_valid = search_database(token)
+    if is_valid is False:
+        raise AccessError(description='Invalid Token')
+    #make a list to store all channels user is in
+    authed_channel = []
+    #get the user's id
+    user = get_u_id(token)
+    #iterate through the list of channels
+    for chan in list_of_channels['channels']:
+        #iterate through list of members
+        for mem in chan['all_members']:
+            #if user are in the channel append to list
+            if mem == user:
+                authed_channel.append(chan)
+    return authed_channel
+def channels_listall(token):
+    """Gets a token and returns a list of channel that exist"""
+    #check for access error
+    is_valid = search_database(token)
+    if is_valid is False:
+        raise AccessError(description='Invalid Token')
+    #no access error means they are user and returns all channels
+    return list_of_channels['channels']
