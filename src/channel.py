cbb58577c80941af1e935ad16472157e387f9c78
Jeremy <jeremyyip98@gmail.com>
2020-03-29 13:59:38 +1100

    Deleted the unsued import message_send

--- src/channel.py
+++ src/channel.py
@@ -4,7 +4,6 @@ channel.py
 Written by Jackie Cai z5259449
 """
 from database import channel_ids, list_of_channels, search_database, get_u_id, get_profile, get_channel, get_message, get_formatted_user
-from message import message_send
 from error import AccessError, InputError
 
 def channel_join(token, channel_id):
