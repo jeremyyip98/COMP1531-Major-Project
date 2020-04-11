656c909c208596d42907af37340dd95372fdf89d
Sean <smccghy@gmail.com>
2020-04-11 11:57:46 +1000

    Added way to save img_url

--- src/image_upload.py
+++ src/image_upload.py
@@ -13,10 +13,11 @@ def upload_profile_pic(token, img_url, x_start, y_start, x_end, y_end):
     r = requests.get(img_url, allow_redirects=True)
     if r.status_code != 200:
         raise InputError(description='Image could not be accessed')    
-    filename = f"profile_pictures/{uuid.uuid4().hex}"
+    filename = uuid.uuid4().hex
+    path = f"profile_pictures/{filename}"
     # Should Check folder is there first
     
-    open(filename, 'wb').write(r.content)
+    open(path, 'wb').write(r.content)
     """ Opens the image using pillow """
     im = Image.open('image')
     if im.format != 'jpeg':
@@ -30,6 +31,6 @@ def upload_profile_pic(token, img_url, x_start, y_start, x_end, y_end):
             raise InputError(description='Crop out of bounds')
     """ Crops the image according to input coordinates """
     new = im.crop(coordinates)    
-    new.save(filename, format='jpg')
-    # Then needs to host url and link the profile picture to user structure
-
+    new.save(path, format='jpeg')
+    # Need to Implement this function
+    database.set_img_url(token, filename)
