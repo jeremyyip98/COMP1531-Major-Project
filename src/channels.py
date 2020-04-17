622a59c78fa05852f602980b1784461fefe83cc1
Jeffrey Yang <jeffrey2999@gmail.com>
2020-04-17 20:51:12 +1000

    added further functionality to hangman

--- src/channels.py
+++ src/channels.py
@@ -36,6 +36,7 @@ def channels_create(token, name, is_public):
         'current_progress_list' : [],
         'current_progress_word' : "",
         'guessed_letters_list' : [],
+        'incorrect_letters_list' : [],
     }
     list_of_channels.append(new_channel)
     return {'channel_id' : re_channel_id}
--- src/hangman.py
+++ src/hangman.py
@@ -6,6 +6,13 @@ site = "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=tex
 response = requests.get(site)
 dictionary = response.content.splitlines()
 
+stages = ['\n\n\n\n_________\nFive tries left',
+'|\n|\n|\n|\n|_________\nFour tries left',
+'_______\n|\n|\n|\n|\n|_________\nThree tries left',
+'_______\n|     O\n|   __|__\n|\n|\n|_________\nTwo tries left',
+'_______\n|     O\n|   __|__\n|     |\n|\n|_________\nOne try left',
+'_______\n|     O\n|   __|__\n|     |\n|    / \ \n|_________\nGame over!']
+
 
 def reset_hangman(channel_id):
     global list_of_channels
@@ -15,6 +22,7 @@ def reset_hangman(channel_id):
             channel['current_progress_list'] = []
             channel['current_progress_word'] = ""
             channel['guessed_letters_list'] = []
+            channel['incorrect_letters_list'] = []
 
 def start_game(channel_id):
     '''starts a game of hangman in a channel'''
@@ -33,9 +41,11 @@ def make_guess(guess, channel_id):
     global list_of_channels
     for channel in list_of_channels:
         if channel['channel_id'] == channel_id:
+            if len(channel['incorrect_letters_list']) == 6:
+                return "You have already lost. Please start a new game"
             for letter in channel['guessed_letters_list']:
                 if letter == guess:
-                    return f"You have already tried this letter\n {channel['current_progress_word']}"
+                    return f"You have already tried this letter\n {channel['current_progress_word']}\nIncorrect:{channel['incorrect_letters_list']}"
             found = False
             channel['guessed_letters_list'].append(guess)
             for i, letter in enumerate(channel['the_word']):
@@ -47,4 +57,6 @@ def make_guess(guess, channel_id):
                 return f"Congratulations!, {channel['the_word']} was the word!"
             if found:
                 return channel['current_progress_word']
-            return f"{guess} is not part of the word!\n {channel['current_progress_word']}"
+            channel['incorrect_letters_list'].append(guess)
+
+            return f"{guess} is not part of the word!\n {stages[len(channel['incorrect_letters_list'])-1]}\n{channel['current_progress_word']}\nIncorrect:{channel['incorrect_letters_list']}"
