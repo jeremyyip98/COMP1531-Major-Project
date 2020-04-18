9aa264d5bfa916389d83f50af385f6e7390438fd
Jeremy Yip <jeremyyip98@gmail.com>
2020-04-18 14:10:43 +1000

    Fixed all of the pylint error

--- src/error.py
+++ src/error.py
@@ -1,9 +1,14 @@
+"""
+error.py
+"""
 from werkzeug.exceptions import HTTPException
 
 class AccessError(HTTPException):
+    """Return access error"""
     code = 400
     message = 'No message specified'
 
 class InputError(HTTPException):
+    """Return input error"
     code = 400
     message = 'No message specified'
