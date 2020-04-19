f0b8527ee3f5beef7aeda1e94e28dab6c882e72e
Jeffrey Yang <jeffrey2999@gmail.com>
2020-04-19 12:35:42 +1000

    made workspace_reset.py pylint compliant

--- src/workspace_reset.py
+++ src/workspace_reset.py
@@ -1,6 +1,13 @@
+'''
+COMP1531
+Workspace reset
+'''
 import database
 
 def workspace_reset():
+    '''
+    Resets the database
+    '''
     database.restore_channel_database()
     database.restore_database()
     database.restore_standup_queue()
